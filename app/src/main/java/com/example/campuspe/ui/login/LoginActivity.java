ccefdc181dcdc9d57c405e6fd21a280d73784a48
Sarthak Agarwal <sarthak.20208110@mnnit.ac.in>
2022-04-22 22:17:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/campuspe/ui/login/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.campuspe.ui.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.campuspe.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
