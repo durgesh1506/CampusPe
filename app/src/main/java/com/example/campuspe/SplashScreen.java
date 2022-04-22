955c8346d6c52e5cb3bdeb8a31d54e27a3d6a0c7
Durgesh Singh <durgesh.20208044@mnnit.ac.in>
2022-04-23 02:51:46 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/campuspe/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.campuspe;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.VideoView;
+
+import com.example.campuspe.ui.login.LoginActivity;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        try {
+            VideoView videoHolder = findViewById(R.id.videoViewRelative);
+
+            Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash);
+            videoHolder.setVideoURI(video);
+
+            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    jump();
+                }
+            });
+            videoHolder.start();
+        } catch (Exception ex) {
+            jump();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        jump();
+        return true;
+    }
+
+    private void jump() {
+        if (isFinishing())
+            return;
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+}
