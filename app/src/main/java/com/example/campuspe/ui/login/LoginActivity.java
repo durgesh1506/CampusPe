e6d1ce28ab4879ebecc85805bb447436f69c5be8
AbhayD11 <abhay.20204001@mnnit.ac.in>
2022-04-23 02:48:56 +0530

    Added Login Functionality

--- app/src/main/java/com/example/campuspe/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/campuspe/ui/login/LoginActivity.java
@@ -1,14 +1,17 @@
 package com.example.campuspe.ui.login;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +19,28 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.campuspe.R;
 import com.example.campuspe.ui.LoggedIn;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button otpBtn;
-
+EditText tv;
+FirebaseAuth mAuth;
+String phoneNo;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,18 +48,95 @@ public class LoginActivity extends AppCompatActivity {
 //        startActivity(new Intent(this, LoggedIn.class));
         getSupportActionBar().hide();
         otpBtn = findViewById(R.id.otpbtn);
+        tv=findViewById(R.id.username);
+        mAuth = FirebaseAuth.getInstance();
+
         otpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                phoneNo = tv.getText().toString();
+
+                registerUser(v);
+                sendVerificationCodeToUser(phoneNo);
                 showDialog();
             }
         });
+    }
+
+    private Boolean validatePhoneNo() {
+        String val = tv.getText().toString();
+
+        if (val.isEmpty()) {
+            tv.setError("Field cannot be empty");
+            return false;
+        } else {
+            tv.setError(null);
+
+            return true;
+        }
+    }
+
+    private void sendVerificationCodeToUser(String phoneNo) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber("+91"+phoneNo)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    public PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks=new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        private static final String TAG ="";
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential credential) {
+
+            Toast.makeText(getApplicationContext(),"Verification Completed",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+            Log.w(TAG, "onVerificationFailed", e);
+
+          Toast.makeText(getApplicationContext(),"Code Was Not Sent",Toast.LENGTH_SHORT).show();
+
+            // Show a message and update the UI
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String verificationId,
+                               @NonNull PhoneAuthProvider.ForceResendingToken token) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            Toast.makeText(getApplicationContext(),"Verification Code Sent Successfully",Toast.LENGTH_SHORT).show();
+
+
+
+        }
+    };
+
+    public void registerUser(View view) {
+
+        // Performing Validation by calling validation functions
+        if (!validatePhoneNo()) {
+            return;
+        }
+
+        //Get the Phone No from phone no field in String
+
+
     }
 
     private void showDialog(){
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.otp_dialog);
+
         dialog.show();
         dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
